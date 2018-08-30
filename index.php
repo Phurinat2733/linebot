618c5d8e99582d91ad94a4ffdaedcf36b245bc19
Phurinat2733 <42757420+Phurinat2733@users.noreply.github.com>
2018-08-30 09:10:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'QoYlV2SNWU962nXM/7iAkLLD73bvlzZFpxvqi29SgqPeeKDt0xFNVEoobjqxNkh8cqRPM5FjqxmeQX+cZxv4Vwg6+SM2iVjNqR7CLWrhVM5w12OR8mTQdyiCSNj0dE53mvOE/3GHgW8keruuXF82AgdB04t89/1O/w1cDnyilFU=';
+$access_token = 'DcVdiO4eO10Ba1aHOV4YvUkRvD5vsC9gNEvWflhwPHS0KRRpANO0mqVzdXsOiaTpu9z7SnZxm7aE6GwYQeqD1Y+YvvoSs6VFfQrgNQoGEa3jOPA9o7j3q+U6GyWgauLBORRoTbXJgtxA5Tx5wx1yBAdB04t89/1O/w1cDnyilFU=';
 $host = "ec2-107-22-211-182.compute-1.amazonaws.com";
 $user = "mmdkvvqziulstc";
 $pass = "e10240d71df70c411f5201bc37491e9091491ff276b8d8b66f8e507ea5b7dc22";
